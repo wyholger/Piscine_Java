52cdcca762be61882dcdc3cab3a102b4410b9874
wyholger <dan154v@gmail.com>
2022-05-18 23:31:33 +0300

    Revert "fix d03 ex00"
    This reverts commit 0395571049054b5545f70fcc95ade66f11d67518.

--- day_03/ex03_maven/src/main/java/com/company/CheckerArgs.java
+++ day_03/ex03_maven/src/main/java/com/company/CheckerArgs.java
@@ -5,8 +5,8 @@ import static com.company.Color.*;
 public class CheckerArgs
 {
 	public static String MSG_ERR_NUM_ARG = "Error. One argument required.";
-	public static String EXAMPLE_ARGS = "Example: java Program --count=50";
-	public static String DETAILS = "Argument == --count=<N>. <N> must be a positive integer.";
+	public static String EXAMPLE_ARGS = "Example: java Program --threadsCount=3";
+	public static String DETAILS = "Arguments == --threadsCount=<K>.\n<K> must be a positive integer more than 0.";
 	public static String MSG_NO_VALID_ARG = "Error. No valid arg.";
 
 	CheckerArgs(String example)
@@ -19,15 +19,21 @@ public class CheckerArgs
 		return args.length == 1;
 	}
 
+	public static boolean check_two_arg(String[] args)
+	{
+		return args.length == 2;
+	}
+
 	public static Integer check_valid_arg(String[] args)
 	{
-		String[] str;
-		int val;
+		String[]	str;
+
+		int val = 0;
 
 		str = args[0].split("=");
 		if (str.length != 2)
 			return null;
-		if (!str[0].equals("--count"))
+		if (!str[0].equals("--threadsCount"))
 			return null;
 		try
 		{
@@ -37,7 +43,7 @@ public class CheckerArgs
 		{
 			return null;
 		}
-		if (val < 0)
+		if (val <= 0)
 			return null;
 		return val;
 	}
